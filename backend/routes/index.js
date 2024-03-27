0def8e8eaee9f68fd459a632e968b89879d8c81b
Harshit Agarwal <ha46@illinois.edu>
2024-03-27 00:21:57 -0500

    start server on 3000 and return preference

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const {preferences} = require("./../model.js");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+/* GET home page. */
+router.get('/preferences', async function(req, res, next) {
+  const questions = await preferences.find({});
+  res.send(questions)
 });
 
 module.exports = router;
